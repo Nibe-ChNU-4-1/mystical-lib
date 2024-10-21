1bdaa148282f6ee7b7d1be082e868b28fa752079
Nibe34 <128199146+Nibe34@users.noreply.github.com>
2024-10-21 21:07:08 +0300

    admin now can change relics

--- backend/homepages.py
+++ backend/homepages.py
@@ -7,7 +7,8 @@ homepages_bp = Blueprint('homepages', __name__)
 @homepages_bp.route('/admin')
 def admin_home():
     if 'user' in session and session['user']['role'] == 'admin':
-        return render_template('admin_home.html')
+        relics = list(relics_collection.find())
+        return render_template('admin_home.html', relics=relics)
     return redirect(url_for('auth.login'))
 
 @homepages_bp.route('/user')
@@ -39,4 +40,26 @@ def take_relic(relic_id):
         flash('This relic is already taken by someone else.')
 
     return redirect(url_for('catalog'))
+
+@homepages_bp.route('/admin/edit_relic/<relic_id>', methods=['GET', 'POST'])
+def edit_relic(relic_id):
+    if 'user' not in session or session['user']['role'] != 'admin':
+        return redirect(url_for('auth.login'))
+
+    relic = relics_collection.find_one({'_id': ObjectId(relic_id)})
+
+    if request.method == 'POST':
+        # Оновлюємо реліквію з отриманими даними
+        updated_data = {
+            'name': request.form['name'],
+            'description': request.form['description'],
+            'available': request.form.get('available') == 'on',
+            'owner': request.form['owner']
+        }
+        relics_collection.update_one({'_id': ObjectId(relic_id)}, {'$set': updated_data})
+        return redirect(url_for('homepages.admin_home'))
+
+    return render_template('edit_relic.html', relic=relic)
+
+
 
